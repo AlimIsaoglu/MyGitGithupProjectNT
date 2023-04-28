205461e5ed21655d7c3aaa1344d0cf337ec3a996
Ali <ali.isaoglu@gmail.com>
2023-04-29 00:02:24 +0200

    ali3

--- src/Account.java
+++ src/Account.java
@@ -2,5 +2,9 @@ public class Account {
     public static void main(String[] args) {
         System.out.println("Bazi codeler yazdim");
         //Ali2
+
+        System.out.println("Evet");
+
+        //Ali3
     }
 }
